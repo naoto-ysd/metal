8ae89ada613762807e645544fe7aa56957a88c2b
naoto-ysd <61533915+naoto-ysd@users.noreply.github.com>
2021-03-20 23:48:11 +0900

    Create controller

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,7 @@
 */
 
 Route::get('/', function () {
-    // return view('welcome');
-    return view('helloworld');
+    return view('welcome');
 });
 
-Route::get('hello', function () {
-    return '<html><body><h1>Hello</h1><p> This is simple page. </p></body></html>';
-});
+Route::get('hello/{id?}/{pass?}', "HelloController@index");
